1667b505ade4261d2b9c55faa4294ba885c015fb
Languzda <langusda@gmail.com>
2025-03-30 13:16:49 +0200

    o2-springboot-devtools

---
+++ 01-spring-boot-overview/O1-spring-boot-demo/src/main/java/com/learnspring/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,13 @@
+package com.learnspring.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappApplication.class, args);
+	}
+
+}
